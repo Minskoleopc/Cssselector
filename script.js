066a8f4b79a1ea9107bea87d4ed34d0689b70cce
unknown <Minskoleopc>
2024-11-25 20:41:43 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+//Select by tagName
+//input
+
+// Selecting the element by id 
+//#username
+
+// Selecting the element by class 
+//.form-input
+
+// Selecting the element by attribute
+// tagName[attribute="value"]
+//input[type= "password"]
+
+// Select by multiple attributes
+//input[type= "password"][name= "email"]
+
+
+// Partial Match on attribute values
+// StartsWith(^=)
+{/* <input type="email-234234"/>
+input[type^="em"] */}
+
+// Ends with($=)
+// <input type = "email-address"/>
+// <input type = "email"/>
+
+
+
+
